54227e096c133d7ceb7f0e6ac88ff48471b188c6
Etienne Lenhart <lenhart@makaio.com>
2017-12-08 18:33:12 +0100

    Use block tags for KDoc documentation

--- eiffel/src/main/java/com/etiennelenhart/eiffel/Status.kt
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/Status.kt
@@ -1,8 +1,9 @@
 package com.etiennelenhart.eiffel
 
 import com.etiennelenhart.eiffel.livedata.Resource
+import com.etiennelenhart.eiffel.result.Result
 
 /**
- * The current status of a command or LiveData [Resource].
+ * The current status of a command's [Result] or LiveData [Resource].
  */
 enum class Status { SUCCESS, PENDING, ERROR }
