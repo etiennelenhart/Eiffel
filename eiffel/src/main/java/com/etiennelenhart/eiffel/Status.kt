6f7e7796427b9110b33e62b527b8ad3539acbf7e
Etienne Lenhart <lenhart@makaio.com>
2017-12-08 17:40:14 +0100

    Add 'Status' enum

---
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/Status.kt
@@ -0,0 +1,8 @@
+package com.etiennelenhart.eiffel
+
+import com.etiennelenhart.eiffel.livedata.Resource
+
+/**
+ * The current status of a command or LiveData [Resource].
+ */
+enum class Status { SUCCESS, PENDING, ERROR }
