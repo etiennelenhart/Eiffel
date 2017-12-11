2694d51165e6f4e1732a7ba8a0d3752bc5d1c551
Etienne Lenhart <lenhart@makaio.com>
2017-12-11 09:36:29 +0100

    Add marker interface for view states

---
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/state/ViewState.kt
@@ -0,0 +1,8 @@
+package com.etiennelenhart.eiffel.state
+
+/**
+ * Marker interface for data classes that represent a view state.
+ *
+ * Implementing classes can be used as state in [StateViewModel].
+ */
+interface ViewState
