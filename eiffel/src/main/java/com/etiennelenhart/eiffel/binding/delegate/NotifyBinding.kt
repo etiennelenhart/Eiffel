d5c5b971467c563fdec50625ad269fb1668f23ea
Etienne Lenhart <lenhart@makaio.com>
2017-12-17 13:35:22 +0100

    Add property delegate to notify property changes to data bindings

---
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/binding/delegate/NotifyBinding.kt
@@ -0,0 +1,29 @@
+package com.etiennelenhart.eiffel.binding.delegate
+
+import android.databinding.BaseObservable
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Property delegate to notify a property change to a data binding.
+ *
+ * The delegating property should be annotated with `@get:Bindable` to generate a field in BR.
+ * Example usage:
+ * ```
+ * @get:Bindable
+ * var sampleValue by NotifyBinding("", BR.sampleValue)
+ * ```
+ *
+ * @param[T] Type of the notifying property.
+ * @param[value] Initial value.
+ * @param[fieldId] The id of the generated BR field.
+ */
+class NotifyBinding<T : Any>(private var value: T, private val fieldId: Int) : ReadWriteProperty<BaseObservable, T> {
+
+    override fun getValue(thisRef: BaseObservable, property: KProperty<*>) = value
+
+    override fun setValue(thisRef: BaseObservable, property: KProperty<*>, value: T) {
+        this.value = value
+        thisRef.notifyPropertyChanged(fieldId)
+    }
+}
