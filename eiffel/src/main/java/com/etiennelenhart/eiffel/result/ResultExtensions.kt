a517285f5774921af939589806775002f7e7c643
Etienne Lenhart <lenhart@makaio.com>
2018-03-08 17:50:19 +0100

    Remove unnecessary 'on' Result extension

--- eiffel/src/main/java/com/etiennelenhart/eiffel/result/ResultExtensions.kt
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/result/ResultExtensions.kt
@@ -3,22 +3,6 @@ package com.etiennelenhart.eiffel.result
 import com.etiennelenhart.eiffel.ErrorType
 
 /**
- * Invokes the respective lambda expression for this result variant and returns the expression's result.
- *
- * @param[T] Type of the result's data.
- * @param[R] The expressions' return type.
- * @param[success] Expression to call for a successful command's result.
- * @param[isPending] Expression to call for a pending command's result.
- * @param[error] Expression to call for a failed command's result.
- * @return The called expression's result.
- */
-inline fun <T, R> Result<T>.on(success: (data: T) -> R, isPending: (data: T) -> R, error: (data: T, type: ErrorType) -> R) = when (this) {
-    is Result.Success -> success(data)
-    is Result.Pending -> isPending(data)
-    is Result.Error -> error(data, type)
-}
-
-/**
  * Invokes the given lambda expression when the command was successful and returns the expression's result.
  *
  * @param[T] Type of the result's data.
--- eiffel/src/test/java/com/etiennelenhart/eiffel/result/ResultExtensionsTest.kt
+++ eiffel/src/test/java/com/etiennelenhart/eiffel/result/ResultExtensionsTest.kt
@@ -8,45 +8,6 @@ import kotlin.test.assertNotEquals
 class ResultExtensionsTest {
 
     @Test
-    fun `GIVEN Result Success with 'data' WHEN 'on' called THEN 'success' is invoked`() {
-        val result = Result.Success("success")
-
-        val actual = result.on(
-            success = { it },
-            isPending = { "pending" },
-            error = { _, _ -> "error" }
-        )
-
-        assertEquals("success", actual)
-    }
-
-    @Test
-    fun `GIVEN Result Pending with 'data' WHEN 'on' called THEN 'isPending' is invoked`() {
-        val result = Result.Pending("pending")
-
-        val actual = result.on(
-            success = { "success" },
-            isPending = { it },
-            error = { _, _ -> "error" }
-        )
-
-        assertEquals("pending", actual)
-    }
-
-    @Test
-    fun `GIVEN Result Error WHEN 'on' called THEN 'error' is invoked`() {
-        val result = Result.Error("error")
-
-        val actual = result.on(
-            success = { "success" },
-            isPending = { "pending" },
-            error = { data, _ -> data }
-        )
-
-        assertEquals("error", actual)
-    }
-
-    @Test
     fun `GIVEN Result Success with 'data' WHEN 'onSuccess' called THEN 'block' is invoked`() {
         val result = Result.Success("block")
 
