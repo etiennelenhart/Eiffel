4c3a96a4c5941ccb49d4c7b7c8a40473031fa847
Etienne Lenhart <lenhart@makaio.com>
2018-06-13 21:20:38 +0200

    Update Kotlin to 1.2.50, Gradle to 3.2.0-alpha18 and Wrapper to 4.8

--- eiffel/src/main/java/com/etiennelenhart/eiffel/binding/delegate/ContentViewBinding.kt
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/binding/delegate/ContentViewBinding.kt
@@ -3,7 +3,7 @@ package com.etiennelenhart.eiffel.binding.delegate
 import android.app.Activity
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
-import android.support.annotation.LayoutRes
+import androidx.annotation.LayoutRes
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
