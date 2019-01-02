d028edeec153598942e4023bbee2d6c5ca541538
Etienne Lenhart <lenhart@makaio.com>
2019-01-02 16:14:23 +0100

    Remove unnecessary if statement in 'NotifyBinding'

--- eiffel/src/main/java/com/etiennelenhart/eiffel/binding/delegate/NotifyBinding.kt
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/binding/delegate/NotifyBinding.kt
@@ -9,8 +9,6 @@ import kotlin.reflect.KProperty
  *
  * The delegating property should be annotated with `@get:Bindable` to generate a field in BR.
  *
- * **Note: Checks for structural inequality before triggering the change.**
- *
  * May be used in a [BaseObservable] like this:
  * ```
  * @get:Bindable
@@ -26,10 +24,8 @@ class NotifyBinding<T : Any>(private var value: T, private val fieldId: Int) : R
     override fun getValue(thisRef: BaseObservable, property: KProperty<*>) = value
 
     override fun setValue(thisRef: BaseObservable, property: KProperty<*>, value: T) {
-        if (this.value != value) {
-            this.value = value
-            thisRef.notifyPropertyChanged(fieldId)
-        }
+        this.value = value
+        thisRef.notifyPropertyChanged(fieldId)
     }
 }
 
@@ -37,8 +33,6 @@ class NotifyBinding<T : Any>(private var value: T, private val fieldId: Int) : R
  * Convenience extension function for the [NotifyBinding] delegate.
  *
  * The delegating property should be annotated with `@get:Bindable` to generate a field in BR.
- *
- * **Note: Checks for structural inequality before triggering the change.**
  *
  * May be used in a [BaseObservable] like this:
  * ```
