67d0ac83f602f33be2db7fd1f81cacb158e73a21
Etienne Lenhart <lenhart@makaio.com>
2019-02-01 12:06:38 +0100

    Simplify test for 'StateObserveProperty'

--- eiffel/src/test/java/com/etiennelenhart/eiffel/state/extension/StateObservePropertyTest.kt
+++ eiffel/src/test/java/com/etiennelenhart/eiffel/state/extension/StateObservePropertyTest.kt
@@ -1,70 +1,38 @@
 package com.etiennelenhart.eiffel.state.extension
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.etiennelenhart.eiffel.state.Action
+import androidx.lifecycle.MutableLiveData
 import com.etiennelenhart.eiffel.state.State
-import com.etiennelenhart.eiffel.state.update
-import com.etiennelenhart.eiffel.viewmodel.EiffelViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 import kotlin.test.assertEquals
 
-@UseExperimental(ExperimentalCoroutinesApi::class)
 class StateObservePropertyTest {
 
     @get:Rule
     val rule: TestRule = InstantTaskExecutorRule()
 
-    @BeforeTest
-    fun before() = Dispatchers.setMain(Dispatchers.Unconfined)
-
-    @AfterTest
-    fun after() = Dispatchers.resetMain()
-
     data class TestState(val count: Int = 0, val other: String = "") : State
 
-    sealed class TestAction : Action {
-        object Increment : TestAction()
-        object Other : TestAction()
-    }
-
-    val testStateUpdate = update<TestState, TestAction> { state, action ->
-        val count = state.count
-        when (action) {
-            TestAction.Increment -> state.copy(count = count + 1)
-            TestAction.Other -> state.copy(other = "changed")
-        }
-    }
-
     @Test
-    fun `GIVEN EiffelViewModel subclass WHEN 'dispatch' changes observed property THEN 'onChange' is invoked`() {
-        @UseExperimental(ExperimentalCoroutinesApi::class)
-        val viewModel =
-            object : EiffelViewModel<TestState, TestAction>(TestState(), testStateUpdate, emptyList(), Dispatchers.Unconfined, Dispatchers.Unconfined) {}
+    fun `GIVEN LiveData with State WHEN observed property changed THEN 'onChange' is invoked`() {
+        val state = MutableLiveData<TestState>().apply { value = TestState() }
 
         var actual = 0
-        viewModel.state.observePropertyForever({ it.count }) { actual = it }
-        viewModel.dispatch(TestAction.Increment)
+        state.observePropertyForever({ it.count }) { actual = it }
+        state.value = state.value!!.copy(count = 1)
 
         assertEquals(1, actual)
     }
 
     @Test
     fun `GIVEN EiffelViewModel subclass WHEN 'dispatch' does not change observed property THEN 'onChange' is not invoked`() {
-        @UseExperimental(ExperimentalCoroutinesApi::class)
-        val viewModel =
-            object : EiffelViewModel<TestState, TestAction>(TestState(), testStateUpdate, emptyList(), Dispatchers.Unconfined, Dispatchers.Unconfined) {}
+        val state = MutableLiveData<TestState>().apply { value = TestState() }
 
         var actual = 0
-        viewModel.state.observePropertyForever({ it.count }) { actual = it }
-        viewModel.dispatch(TestAction.Other)
+        state.observePropertyForever({ it.count }) { actual = it }
+        state.value = state.value!!.copy(other = "changed")
 
         assertEquals(0, actual)
     }
