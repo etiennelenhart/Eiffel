481bee42f09fb5ec2222b8fa90462247cc6ad05d
Etienne Lenhart <lenhart@makaio.com>
2019-05-13 13:42:11 +0200

    Bindable property as non-null LiveData to prevent safeUnbox() warning

--- eiffel/src/main/java/com/etiennelenhart/eiffel/util/LiveDataExtensions.kt
+++ eiffel/src/main/java/com/etiennelenhart/eiffel/util/LiveDataExtensions.kt
@@ -1,8 +1,19 @@
 package com.etiennelenhart.eiffel.util
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
+
+class NonNullMediatorLiveData<T : Any> : MediatorLiveData<T>() {
+
+    override fun getValue() = super.getValue()!!
+
+    fun observe(owner: LifecycleOwner, onChanged: (T) -> Unit) = observe(owner, Observer<T> { it?.let(onChanged) })
+}
+
+internal fun <T : Any> LiveData<T>.nonNull(): NonNullMediatorLiveData<T> {
+    return NonNullMediatorLiveData<T>().apply {
+        addSource(this@nonNull) { if (it != null) value = it }
+    }
+}
 
 internal fun <T> LiveData<T>.distinctUntilChanged(): LiveData<T> {
     return MediatorLiveData<T>().apply {
