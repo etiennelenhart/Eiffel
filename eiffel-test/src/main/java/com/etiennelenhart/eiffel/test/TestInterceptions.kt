653f274851d3335a90957c0033f4489783c9dc39
Etienne Lenhart <lenhart@makaio.com>
2019-05-17 11:07:28 +0200

    Change 'testInterceptions' to a suspending extension function

--- eiffel-test/src/main/java/com/etiennelenhart/eiffel/test/TestInterceptions.kt
+++ eiffel-test/src/main/java/com/etiennelenhart/eiffel/test/TestInterceptions.kt
@@ -6,25 +6,29 @@ import com.etiennelenhart.eiffel.interception.Next
 import com.etiennelenhart.eiffel.state.Action
 import com.etiennelenhart.eiffel.state.State
 import com.etiennelenhart.eiffel.viewmodel.EiffelViewModel
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.coroutineScope
 
 /**
  * Allows testing a chain of [Interceptions] without an instance of [EiffelViewModel].
  *
+ * @receiver Chain of [Interception] instances to test.
  * @param[S] Type of [State] to test with.
  * @param[A] Type of [Action] to test with.
- * @param[interceptions] Chain of [Interception] instances to test.
  * @param[currentState] Current [State] to pass to the chain.
  * @param[action] [Action] to be processed by the chain.
  * @return [Action] resulting from processing, may be null.
  */
-fun <S : State, A : Action> testInterceptions(interceptions: Interceptions<S, A>, currentState: S, action: A) = runBlocking {
-    fun next(index: Int): Next<S, A> = if (index == interceptions.size) {
-        { _, _, action, _ -> action }
-    } else {
-        { scope, state, action, dispatch ->
-            interceptions[index].run {
-                invoke(scope, state, action, dispatch, next(index + 1))
+suspend fun <S : State, A : Action> Interceptions<S, A>.applyOn(currentState: S, action: A) = coroutineScope {
+    val interceptions = this@applyOn
+
+    fun next(index: Int): Next<S, A> {
+        return if (index == interceptions.size) {
+            { _, _, action, _ -> action }
+        } else {
+            { scope, state, action, dispatch ->
+                interceptions[index].run {
+                    invoke(scope, state, action, dispatch, next(index + 1))
+                }
             }
         }
     }
